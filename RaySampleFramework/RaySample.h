9471523f367e7998b98060c49d1b0686eaeb0c4f
MegaPixel79 <46072573+MegaPixel79@users.noreply.github.com>
2019-05-29 12:23:18 +0200

    Started adding d3d12 initialization code

--- RaySampleFramework/RaySample.h
+++ RaySampleFramework/RaySample.h
@@ -3,6 +3,11 @@
 #include "RayPCH.h"
 #include "RayWin32Application.h"
 
+//forward declaration to the renderer interface (virtualizes the underlying API)
+//This will let us to specify even a different API other than DX12. Like Vulkan for example.
+//TODO: uncomment when DX12 will be moved to the hardware interface
+//class Ray_IHardwareRenderer;
+
 class Ray_Sample
 {
 public:
@@ -36,12 +41,46 @@ protected:
 
 	float mAspectRatio = 0.0f;
 
+	/** A pointer to the hardware renderer. It virtualizes the underlying graphics API */
 
-	/** Add device related pointer to implementation */
+	//TODO: Move the DX12 implementation in the hardware interface class. Do the same for any other API.
+	//std::unique_ptr<Ray_IHardwareRenderer> mHardwareRenderer;	
+
 
 private:
 
+	template<typename T>
+	using ComPtr = Microsoft::WRL::ComPtr<T>;
 
+	//DX12 specific interfaces/////////////////////////////////
+
+	static const size_t kMAX_BACK_BUFFER_COUNT = 3;
+
+	/** Index to the current back buffer */
+	u32 mBackBufferIndex = 0;
+
+	/** Interface to the installed graphics adapter */
+	ComPtr<IDXGIAdapter1> mAdapter;
+
+	/** A description of the graphics adapter       */
+	std::wstring mAdapterDescription;
+
+	/** D3D12 Device */
+	ComPtr<ID3D12Device> mD3DDevice;
+
+	/** D3D12 command queue */
+	ComPtr<ID3D12CommandQueue> mD3DCommandQueue;
+
+	/** D3D12 command list */
+	ComPtr<ID3D12CommandList> mD3DCommandList;
+	
+	/** D3D12 Command allocator */
+	ComPtr<ID3D12CommandAllocator> mD3DCommandAllocator;
+
+
+
+
+	///////////////////////////////////////////////////////////
 
 
 	/** The name of the sample that is shown on the window title */
